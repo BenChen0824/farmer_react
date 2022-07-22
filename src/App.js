bbd95e2a61ea6435a4d824310372791380025e23
BenChen0824 <104603512+BenChen0824@users.noreply.github.com>
2022-07-22 09:02:52 +0800

    那個gitkeep裡面有資料就可以刪掉了喔 請自己操作自己的資料夾就好<3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import './App.css';
+import { BrowserRouter, Route, Router } from 'react-router-dom';
 
 function App() {
-    return <></>;
+    return (
+        <>
+            <BrowserRouter>
+                <Router>
+                    <Route path="/" />
+                    <Route path="/comment" element={<></>} />
+                    {/* 慧敏 */}
+                    <Route path="/recipe" element={<></>} />
+                    {/* 宸睿 */}
+                    <Route path="/product" element={<></>} />
+                    {/* 昱蓉 */}
+                    <Route path="/member" element={<></>} />
+                    {/* 柏安 */}
+                    <Route path="/cart" element={<></>} />
+                    {/* 宗佑 */}
+                    <Route path="/customized_lunch" element={<></>} />
+                    <Route path="/customer_server" element={<></>} />
+                    {/* 阿鑫 */}
+                    <Route path="/game" element={<></>} />
+                    {/* po */}
+                    <Route path="/activity" element={<></>} />
+                    <Route path="/supplier" element={<></>} />
+                    {/* pin */}
+                </Router>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
