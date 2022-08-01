57d0941a3bd86899fc40879a1bb29c2eff953f71
hsieh435 <106055247+hsieh435@users.noreply.github.com>
2022-08-01 15:33:50 +0800

    Update App.js

--- src/component/hsieh/App.js
+++ src/component/hsieh/App.js
@@ -1,25 +1,33 @@
-import { Route, Routes } from 'react-router-dom';
+import './App.css'
+import Nav from './component/Navbar/Nav'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './component/footer/Footer'
+import Recipesearch from './component/hsieh/Recipesearch/Recipesearch'
+import Eachrecipe from './component/hsieh/Eachrecipe/Eachrecipe'
+import Createrecipe from './component/hsieh/Createrecipe/Createrecipe'
+import Popup from './component/hsieh/Recipesearch/Popup'
+import Updaterecipe from './component/hsieh/Createrecipe/Updaterecipe'
 
-import Recipesearch from './Recipesearch/Recipesearch';
-import Eachrecipe from './Eachrecipe/Eachrecipe';
-import Creatrecipe from './Creatrecipe/Creatrecipe';
-import Popup from './Recipesearch/Popup';
-import Updaterecipe from './Creatrecipe/Updaterecipe';
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" />
 
-function RecipeMain() {
-    return (
-        <>
-            <Routes>
-                <Route path="/" element={<Recipesearch />} />
+          <Route path="/recipe" element={<Recipesearch />} />
+          <Route path="/popup" element={<Popup />} />
+          <Route path="/each" element={<Eachrecipe />} />
+          <Route path="/createrecipe" element={<Createrecipe />} />
+          <Route path="/updaterecipe" element={<Updaterecipe />} />
+          {/* 宸睿 */}
+        </Routes>
 
-                <Route path="/popup" element={<Popup />} />
-                <Route path="/each" element={<Eachrecipe />} />
-                <Route path="/creatrecipe" element={<Creatrecipe />} />
-                <Route path="/updaterecipe" element={<Updaterecipe />} />
-                {/* 宸睿 */}
-            </Routes>
-        </>
-    );
+        <Footer />
+      </BrowserRouter>
+    </>
+  )
 }
 
-export default RecipeMain;
+export default App
