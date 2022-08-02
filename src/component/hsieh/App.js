9e436d31fc38f1c33d0f552ea8bc71305e4762b5
huimin <M9805544@gmail.com>
2022-08-02 11:14:41 +0800

    AAA

--- src/component/hsieh/App.js
+++ src/component/hsieh/App.js
@@ -1,33 +1,33 @@
-import './App.css'
-import Nav from './component/Navbar/Nav'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Footer from './component/footer/Footer'
-import Recipesearch from './component/hsieh/Recipesearch/Recipesearch'
-import Eachrecipe from './component/hsieh/Eachrecipe/Eachrecipe'
-import Creatrecipe from './component/hsieh/Creatrecipe/Creatrecipe'
-import Popup from './component/hsieh/Recipesearch/Popup'
-import Updaterecipe from './component/hsieh/Creatrecipe/Updaterecipe'
+import './App.css';
+import Nav from './component/Navbar/Nav';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './component/footer/Footer';
+import Recipesearch from './component/hsieh/Recipesearch/Recipesearch';
+import Eachrecipe from './component/hsieh/Eachrecipe/Eachrecipe';
+import Creatrecipe from './component/hsieh/Creatrecipe/Creatrecipe';
+import Popup from './component/hsieh/Recipesearch/Popup';
+import Updaterecipe from './component/hsieh/Creatrecipe/Updaterecipe';
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route path="/" />
+    return (
+        <>
+            <BrowserRouter>
+                <Nav />
+                <Routes>
+                    <Route path="/" />
 
-          <Route path="/recipe" element={<Recipesearch />} />
-          <Route path="/popup" element={<Popup />} />
-          <Route path="/each" element={<Eachrecipe />} />
-          <Route path="/creatrecipe" element={<Creatrecipe />} />
-          <Route path="/updaterecipe" element={<Updaterecipe />} />
-          {/* 宸睿 */}
-        </Routes>
+                    <Route path="/recipe" element={<Recipesearch />} />
+                    <Route path="/popup" element={<Popup />} />
+                    <Route path="/each" element={<Eachrecipe />} />
+                    <Route path="/creatrecipe" element={<Creatrecipe />} />
+                    <Route path="/updaterecipe" element={<Updaterecipe />} />
+                    {/* 宸睿 */}
+                </Routes>
 
-        <Footer />
-      </BrowserRouter>
-    </>
-  )
+                <Footer />
+            </BrowserRouter>
+        </>
+    );
 }
 
-export default App
+export default App;
