c43ed93180f0672fde64f3f09d97f7979e7514a6
BenChen0824 <104603512+BenChen0824@users.noreply.github.com>
2022-08-02 08:56:14 +0800

    0802

--- src/component/hsieh/App.js
+++ src/component/hsieh/App.js
@@ -1,33 +1,25 @@
-import './App.css'
-import Nav from './component/Navbar/Nav'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Footer from './component/footer/Footer'
-import Recipesearch from './component/hsieh/Recipesearch/Recipesearch'
-import Eachrecipe from './component/hsieh/Eachrecipe/Eachrecipe'
-import Createrecipe from './component/hsieh/Createrecipe/Createrecipe'
-import Popup from './component/hsieh/Recipesearch/Popup'
-import Updaterecipe from './component/hsieh/Createrecipe/Updaterecipe'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Recipesearch from './Recipesearch/Recipesearch';
+import Eachrecipe from './Eachrecipe/Eachrecipe';
+import Createrecipe from './Createrecipe/Createrecipe';
+import Popup from './Recipesearch/Popup';
+import Updaterecipe from './Createrecipe/Updaterecipe';
 
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route path="/" />
-
-          <Route path="/recipe" element={<Recipesearch />} />
-          <Route path="/popup" element={<Popup />} />
-          <Route path="/each" element={<Eachrecipe />} />
-          <Route path="/createrecipe" element={<Createrecipe />} />
-          <Route path="/updaterecipe" element={<Updaterecipe />} />
-          {/* 宸睿 */}
-        </Routes>
-
-        <Footer />
-      </BrowserRouter>
-    </>
-  )
+function RecipeMain() {
+    return (
+        <>
+            
+                <Routes>
+                    <Route path="/" element={<Recipesearch />} />
+                    <Route path="/popup" element={<Popup />} />
+                    <Route path="/each" element={<Eachrecipe />} />
+                    <Route path="/createrecipe" element={<Createrecipe />} />
+                    <Route path="/updaterecipe" element={<Updaterecipe />} />
+                    {/* 宸睿 */}
+                </Routes>
+            
+        </>
+    );
 }
 
-export default App
+export default RecipeMain;
