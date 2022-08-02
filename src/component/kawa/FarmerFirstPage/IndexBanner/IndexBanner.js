280c90911386ed73369f63273cf21fc87367632e
huimin <M9805544@gmail.com>
2022-08-02 15:19:30 +0800

    首頁大圖輪播

--- src/component/kawa/FarmerFirstPage/IndexBanner/IndexBanner.js
+++ src/component/kawa/FarmerFirstPage/IndexBanner/IndexBanner.js
@@ -1,28 +1,109 @@
-import React from "react";
-// import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-// import background from "../../public/images/main_banner_05.jpg";
-import "./IndexBanner.css"
+import React, { Component } from 'react';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-const IndexBanner = () => {
+import './IndexBanner.css';
+import Slider from 'react-slick';
 
-    return (
-        <>
-            <div className="indexBanner_imgwrap">
-                <img src="./images/main_banner_05.jpg" alt="" />
+// const IndexBanner = () => {
+//   return (
+//     <>
+//       <div>
+//         <div className="indexBanner_imgwrap">
+//           <img src="./images/main_banner_05.jpg" alt="" />
+//         </div>
+
+//         <div className="indexBanner_text_area">
+//           <h2 className="pb-3">食在安心。</h2>
+//           <p>
+//             ローズウッド仕上げの⽊製キャビネット
+//             <br />
+//             オンラインストア限定
+//           </p>
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
+
+export default class Fade extends Component {
+    render() {
+        const settings = {
+            dots: true,
+            fade: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+        };
+        return (
+            <div>
+                {/* <h2>Fade</h2> */}
+                <Slider {...settings}>
+                    <div>
+                        <div className="indexBanner_imgwrap">
+                            <img src="./images/main_banner_05.jpg" alt="" />
+                        </div>
+
+                        <div className="indexBanner_text_area">
+                            <h2 className="pb-3">食在安心。1</h2>
+                            <p>
+                                ローズウッド仕上げの⽊製キャビネット
+                                <br />
+                                オンラインストア限定
+                            </p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="indexBanner_imgwrap">
+                            <img src="./images/main_banner_04.jpg" alt="" />
+                        </div>
+
+                        <div className="indexBanner_text_area">
+                            <h2 className="pb-3">食在安心。2</h2>
+                            <p>
+                                ローズウッド仕上げの⽊製キャビネット
+                                <br />
+                                オンラインストア限定
+                            </p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="indexBanner_imgwrap">
+                            <img src="./images/main_banner_03.jpg" alt="" />
+                        </div>
+
+                        <div className="indexBanner_text_area">
+                            <h2 className="pb-3">食在安心。3</h2>
+                            <p>
+                                ローズウッド仕上げの⽊製キャビネット
+                                <br />
+                                オンラインストア限定
+                            </p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="indexBanner_imgwrap">
+                            <img src="./images/main_banner_02.jpg" alt="" />
+                        </div>
+
+                        <div className="indexBanner_text_area">
+                            <h2 className="pb-3">食在安心。4</h2>
+                            <p>
+                                ローズウッド仕上げの⽊製キャビネット
+                                <br />
+                                オンラインストア限定
+                            </p>
+                        </div>
+                    </div>
+                </Slider>
             </div>
+        );
+    }
+}
 
-            <div className="indexBanner_text_area">
-                <h2 className="pb-3">食在安心。</h2>
-                <p>
-                    ローズウッド仕上げの⽊製キャビネット
-                    <br />
-                    オンラインストア限定
-                </p>
-            </div>
-        </>
-    );
-};
-
-export default IndexBanner;
+// export default IndexBanner
