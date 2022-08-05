4375b8caf8631229a3a48446889bb6ad820d14b0
chenjui <hsieh435@gmail.com>
2022-08-05 13:56:09 +0800

    0805

--- src/component/hsieh/Eachrecipe/Eachrecipe.js
+++ src/component/hsieh/Eachrecipe/Eachrecipe.js
@@ -8,7 +8,7 @@ function Eachrecipe() {
             <hr className="line" align="center" />
 
             <div className="recipeinfo">
-                <div className="pic">
+                <div className="pictureineachrecipe">
                     <img src="/images/dishimage.jpg" alt="" />
                 </div>
                 <div className="recipedetail">
