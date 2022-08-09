97aa8281649948be89e3128f494226425330abf8
“Alex <alex781014@gmail.com>
2022-08-09 16:34:29 +0800

    a

--- src/component/xin/Customized_service/Chat/ChatContainer/ChatHeader.jsx
+++ src/component/xin/Customized_service/Chat/ChatContainer/ChatHeader.jsx
@@ -1,28 +1,28 @@
-import React from "react";
+import React from 'react';
 
-const ChatHeader = ({user})=>{
+const ChatHeader = ({ user }) => {
     return (
-      <div className="align-items-start py-2 px-4 w-100 border-bottom border-info d-lg-block sticky-top bg-white">
-        <div className="d-flex align-items-center py-1">
-          <div className="position-relative">
-            <img
-              src={
-                user.username === "管理員"
-                  ? "images/Logo.png"
-                  : "http://bootdey.com/img/Content/avatar/avatar1.png"
-              }
-              className="rounded-circle mx-2"
-              alt={user.username}
-              width="40"
-              height="40"
-            />
-          </div>
-          <div className="flex-grow-1">
-            <strong>{user.username}</strong>
-          </div>
+        <div className="align-items-start py-2 px-4 w-100 border-bottom border-info d-lg-block sticky-top bg-white">
+            <div className="d-flex align-items-center py-1">
+                <div className="position-relative">
+                    <img
+                        src={
+                            user.username === '管理員'
+                                ? 'logo/fresh_LOGO_icon.svg'
+                                : 'http://bootdey.com/img/Content/avatar/avatar1.png'
+                        }
+                        className="rounded-circle mx-2"
+                        alt={user.username}
+                        width="40"
+                        height="40"
+                    />
+                </div>
+                <div className="flex-grow-1">
+                    <strong>{user.username}</strong>
+                </div>
+            </div>
         </div>
-      </div>
     );
-}
+};
 
-export default ChatHeader 
+export default ChatHeader;
