f445602fb3dc0202598f54a727c3b059ee2e2587
Alex <alex781014@gmail.com>
2022-08-11 11:45:25 +0800

    a

--- src/component/xin/Customized_service/Chat/ChatContainer/ChatBody.jsx
+++ src/component/xin/Customized_service/Chat/ChatContainer/ChatBody.jsx
@@ -1,41 +1,44 @@
-import React from "react";
-// import {Waypoint} from "react-waypoint";
+import React, { useEffect, useRef } from 'react';
 
-const ChatBody = ({user,messages})=>{
-        return(
-            <div className="position-relative overflow-auto chat-height">
-                {/* <ScrollableFeed> */}
-                {/* <Waypoint>
-  onEnter={user} */}
-
-                <div className="p-4 d-flex flex-column"> 
-                    {messages.map((message,index) => { 
-                        return message.type === "userStatus" ? (
-                          <div key={index} className="text-center"></div>
-                        ) : (
-                          <div
+const ChatBody = ({ user, messages }) => {
+    const scroll = useRef();
+    //滾動到最後訊息
+    useEffect(() => {
+        scroll.current?.scrollIntoView({ behavior: 'smooth' });
+    }, [messages]);
+    return (
+        <div className="position-relative overflow-auto chat-height ">
+            <div className="p-4 d-flex flex-column">
+                {messages.map((message, index) => {
+                    return message.type === 'userStatus' ? (
+                        <div key={index} className="text-center"></div>
+                    ) : (
+                        <div
                             key={index}
                             className={
-                              message.userId === user.userId
-                                ? "chat-message-right mb-4 xin-bg-color bg-gradient rounded"
-                                : "chat-message-left mb-4 bg-light rounded"
+                                message.userId === user.userId
+                                    ? 'chat-message-right mb-4 xin-bg-color bg-gradient rounded'
+                                    : 'chat-message-left mb-4 bg-light rounded'
                             }
-                          >
-                            <div className="flex-shrink-1  py-2 px-3 ml-3">
-                              {message.message.includes("http") ? (
-                                <a href={message.message}>{message.message}</a>
-                              ) : (
-                                message.message
-                              )}
+                        >
+                            <div
+                                className="flex-shrink-1  py-2 px-3 ml-3"
+                                ref={scroll}
+                            >
+                                {message.message.includes('http') ? (
+                                    <a href={message.message}>
+                                        {message.message}
+                                    </a>
+                                ) : (
+                                    message.message
+                                )}
                             </div>
-                          </div>
-                        );
-                    })}
-                </div>
-                {/* </ScrollableFeed> */}
-                  {/* </Waypoint> */}
+                        </div>
+                    );
+                })}
             </div>
-    )
-}
+        </div>
+    );
+};
 
-export default ChatBody
+export default ChatBody;
