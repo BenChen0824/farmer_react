bf125b649195bd6823cc25215a27814dc6dacd3e
huimin <M9805544@gmail.com>
2022-08-18 15:28:19 +0800

    訂閱功能

--- src/component/kawa/FarmerFirstPage/ContactUs/ContactUs.js
+++ src/component/kawa/FarmerFirstPage/ContactUs/ContactUs.js
@@ -1,72 +1,59 @@
+import { async } from 'emoji-mart';
+import { data } from 'jquery';
 import React, { useRef } from 'react';
 import './ContactUs.css';
-import emailjs from '@emailjs/browser';
-
-// const ContactUs = () => {
-//     function sendEmail() {}
-
-//     return (
-//         <>
-//             <div className="ContactUs_section">
-//                 <h2 className="mb-5">訂閱我們</h2>
-
-//                 <div className="ContactUs_input_area">
-//                     <input placeholder="請輸入信箱" />
-//                     <button onSubmit={sendEmail}>送出</button>
-//                 </div>1
-//             </div>
-//         </>
-//     );
-// };
-
+// import emailjs from '@emailjs/browser';
 
 export const ContactUs = () => {
-    const form = useRef();
+    // const form = useRef();
 
-    const sendEmail = (e) => {
-        e.preventDefault();
+    // const sendEmail = (e) => {
+    //     e.preventDefault();
 
-        emailjs
-            .sendForm(
-                'kawa',
-                'template_dq55ygg',
-                form.current,
-                '56_l3kJvCZE4gQUlM'
-            )
-            .then(
-                (result) => {
-                    console.log(result.text);
-                    console.log('message send');
-                },
-                (error) => {
-                    console.log(error.text);
-                }
-            );
-        e.target.reset();
+    //     emailjs
+    //         .sendForm(
+    //             'kawa', //Gmail /Service ID
+    //             'template_dq55ygg', //Email Templates /Templates ID
+    //             form.current,
+    //             '56_l3kJvCZE4gQUlM' //API keys /Public Key
+    //         )
+    //         .then(
+    //             (result) => {
+    //                 console.log(result.text);
+    //                 console.log('message send');
+    //             },
+    //             (error) => {
+    //                 console.log(error.text);
+    //             }
+    //         );
+    //     e.target.reset();
+    // };
+
+    const sendMail = async () => {
+        const r = await fetch('http://localhost:3600/comment/subscribe', {
+            method: 'post',
+        });
+        const obj = await r.json();
+        console.log(obj);
     };
 
     return (
         // <form ref={form} onSubmit={sendEmail}>
-        //   <label>Name</label>
-        //   <input type="text" name="user_name" />
-        //   <label>Email</label>
-        //   <input type="email" name="user_email" />
-        //   <label>Message</label>
-        //   <textarea name="message" />
-        //   <input type="submit" value="Send" />
-        // </form>
 
-        <form ref={form} onSubmit={sendEmail}>
-            <div className="ContactUs_section">
-                <h2 className="mb-3">訂閱我們</h2>
-                <h5 className="mb-5">已追蹤更多消息!</h5>
+        <div className="ContactUs_section">
+            <h2 className="mb-3">訂閱我們</h2>
+            <h5 className="mb-5">已追蹤更多消息!</h5>
 
-                <div className="ContactUs_input_area">
-                    <input placeholder="請輸入信箱......" name="message" />
-                    <input type="submit" value="送出" className="sendButton" />
-                </div>
+            <div className="ContactUs_input_area">
+                <input placeholder="請輸入信箱......" name="message" />
+                <button
+                    type="button"
+                    value="送出"
+                    className="sendButton"
+                    onClick={sendMail}
+                />
             </div>
-        </form>
+        </div>
     );
 };
 
